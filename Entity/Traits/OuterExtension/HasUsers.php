f10fba5bfa42f5bf2540216f80535a78b256e992
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2017-03-14 15:41:29 +0100

    Fixed wrong namespace in HasUsers trait

--- Entity/Traits/OuterExtension/HasUsers.php
+++ Entity/Traits/OuterExtension/HasUsers.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Librinfo\SonataSyliusUserBundle\Entity\OuterExtension;
+namespace Librinfo\SonataSyliusUserBundle\Entity\Traits\OuterExtension;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
